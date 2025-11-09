ac2de48470a617c795c87e9c9b0a37e301f9562a
Joaquin Alegre <joaco12-2002@hotmail.com>
2025-11-09 11:33:12 -0300

    agregue efectos de scroll a la mayoria de los componentes del lado del hincha

--- Gestion-torneo-futbol-front/src/app/app.ts
+++ Gestion-torneo-futbol-front/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { AfterViewInit, Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./component/header/header";
 import { Footer } from "./component/footer/footer";
@@ -10,17 +10,17 @@ import * as AOS from 'aos';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit{
+export class App implements AfterViewInit{
   protected readonly title = signal('Goal Manager');
 
-  ngOnInit(): void {
-
-    AOS.init({
-      duration: 1000, 
-      once: true      
-    });
+ngAfterViewInit(): void {
+    setTimeout(() => {
+      AOS.init({
+        duration: 1000, 
+        once: false      
+      });
+    }, 100); 
   }
-
 
 }
 
