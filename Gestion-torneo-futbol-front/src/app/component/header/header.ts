6a10c299b03c8b0dc8671a55e0b1f01cd190f52b
Joaquin Alegre <joaco12-2002@hotmail.com>
2025-11-19 00:51:26 -0300

    cambio

--- Gestion-torneo-futbol-front/src/app/component/header/header.ts
+++ Gestion-torneo-futbol-front/src/app/component/header/header.ts
@@ -10,56 +10,153 @@ import { TranslocoPipe, TranslocoService } from '@ngneat/transloco';
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
+// export class Header {
+
+//   isScrolled = false;
+//   isMenuCollapsed = true;
+//   menuVisible: boolean = false;
+
+//     constructor(@Inject(DOCUMENT) private document: Document,
+//                 private translocoService: TranslocoService,
+//                 private router: Router) {}
+
+// @HostListener('document:click', ['$event'])
+//   onDocumentClick(event: MouseEvent): void {
+
+//     if (this.menuVisible) { 
+//       const targetElement = event.target as HTMLElement;
+//       const menuDesplegable = document.getElementById('menu-desplegable');
+//       const menuButton = document.querySelector('.navbar-toggler'); 
+
+//       if (menuButton && menuDesplegable && 
+//           !menuButton.contains(targetElement) && 
+//           !menuDesplegable.contains(targetElement)) {
+        
+//         this.menuVisible = false;
+//       }
+//     }
+//   }
+
+//   toggleMenu() {
+//     this.menuVisible = !this.menuVisible;
+//   }
+
+//   cambiarIdioma(lang: string) {
+//     this.translocoService.setActiveLang(lang);
+//   }
+
+
+//   toggleDarkMode(): void {
+
+//     this.document.body.classList.toggle('dark-mode');
+//     this.isMenuCollapsed = true;
+
+//   }
+
+//  goTo(section: string) {
+//     this.router.navigate(['/']).then(() => {
+//       const el = document.getElementById(section);
+//       if (el) {
+//         el.scrollIntoView({ behavior: 'smooth' });
+//       }
+//     });
+//   }
+
+// }
+
 export class Header {
 
-  isScrolled = false;
-  isMenuCollapsed = true;
-  menuVisible: boolean = false;
+
 
     constructor(@Inject(DOCUMENT) private document: Document,
+
                 private translocoService: TranslocoService,
+
                 private router: Router) {}
 
-@HostListener('document:click', ['$event'])
-  onDocumentClick(event: MouseEvent): void {
 
-    if (this.menuVisible) { 
-      const targetElement = event.target as HTMLElement;
-      const menuDesplegable = document.getElementById('menu-desplegable');
-      const menuButton = document.querySelector('.navbar-toggler'); 
 
-      if (menuButton && menuDesplegable && 
-          !menuButton.contains(targetElement) && 
-          !menuDesplegable.contains(targetElement)) {
-        
-        this.menuVisible = false;
-      }
+  isScrolled = false;
+
+
+
+ 
+
+  public isMenuCollapsed = true;
+
+
+
+  @HostListener('window:scroll', [])
+
+  onWindowScroll() {
+
+
+
+    if (window.scrollY > 10) {
+
+      this.isScrolled = true;
+
+    } else {
+
+
+
+      this.isScrolled = false;
+
     }
+
   }
+
+
+
+  menuVisible = false;
+
+
 
   toggleMenu() {
+
     this.menuVisible = !this.menuVisible;
+
   }
 
+
+
   cambiarIdioma(lang: string) {
+
     this.translocoService.setActiveLang(lang);
+
   }
+
+
+
 
 
   toggleDarkMode(): void {
 
+
+
     this.document.body.classList.toggle('dark-mode');
+
     this.isMenuCollapsed = true;
 
+
+
   }
+
+
 
  goTo(section: string) {
-    this.router.navigate(['/']).then(() => {
-      const el = document.getElementById(section);
-      if (el) {
-        el.scrollIntoView({ behavior: 'smooth' });
-      }
-    });
-  }
 
+    this.router.navigate(['/']).then(() => {
+
+      const el = document.getElementById(section);
+
+      if (el) {
+
+        el.scrollIntoView({ behavior: 'smooth' });
+
+      }
+
+    });
+
+  }
 }
